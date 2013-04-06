be2d739e3e636c457911223bbea66d206c9dced1
Makis Tracend <makis.tracend@gmail.com>
2013-04-06 02:46:07 -0700

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function (grunt) {
+	/*
+	grunt.initConfig({
+		pkg: '<json:package.json>',
+		cdn: {
+			dist: {
+				src: ['./static/*.html', './static/*.css', './static/*.soy'],
+				dest: './dist/static/',
+				cdn: 'http://cdn.cloudfront.net/container/'
+			}
+		}
+	});
+	*/
+	
+	//grunt.loadNpmTasks('grunt-three-json');
+	
+};
