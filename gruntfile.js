5e1f756ff0be1d2fd08d29415f9ee509895a7d37
Makis Tracend <makis.tracend@gmail.com>
2013-05-14 03:56:41 -0700

    fixing default tasks

--- gruntfile.js
+++ gruntfile.js
@@ -1,17 +1,34 @@
+/*
+ * grunt-3d
+ * Source: http://github.com/makesites/grunt-3d
+ *
+ * Created by [Makis Tracend]( [@tracend](http://github.com/tracend) )
+ *
+ * Released under the [MIT license](http://makesites.org/licenses/MIT)
+ */
+
+'use strict';
+
 module.exports = function (grunt) {
-	/*
+
 	grunt.initConfig({
-		pkg: '<json:package.json>',
-		cdn: {
-			dist: {
-				src: ['./static/*.html', './static/*.css', './static/*.soy'],
-				dest: './dist/static/',
-				cdn: 'http://cdn.cloudfront.net/container/'
-			}
+		pkg: grunt.file.readJSON('package.json'),
+
+		// Configuration to be run
+		three_obj: {
+			options: {
+				minify: false
+			},
+			 dist: {
+				 src: ['./static/*.obj', './media/*.obj', './raw/*.obj'],
+				/** @optional  - if provided the converted files will be saved in this folder instead */
+				dest: './assets/'
+			 }
 		}
+
 	});
-	*/
-	
+
+
 	grunt.loadNpmTasks('grunt-three-obj');
-	
+
 };
