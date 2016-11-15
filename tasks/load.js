f7eecc739dd8f4e4e59b753f5068fc549d95f34d
Makis Tracend <makis.tracend@gmail.com>
2016-11-14 17:26:47 -0800

    :zap: grunt-three-obj task location

--- tasks/load.js
+++ tasks/load.js
@@ -1,4 +1,6 @@
-var three_obj = require("grunt-three-obj");
+var fs = require('fs'),
+	path = require('path'),
+	three_obj = require("grunt-three-obj");
 
 module.exports = function (grunt) {
 
@@ -6,6 +8,31 @@ module.exports = function (grunt) {
 	// (better discovery?)
 	// grunt-three-obj
 	//three_obj( grunt );
-	grunt.loadTasks( require("path").join( __dirname, '../node_modules/grunt-three-obj/tasks') );
+
+	var dir_nested = path.join( __dirname, '../node_modules/grunt-three-obj/tasks');
+	var dir_flat = path.join( __dirname, '../../grunt-three-obj/tasks');
+	var dir;
+	// check task location
+	if( isDir(dir_nested) ){
+		dir = dir_nested;
+	} else if( isDir(dir_flat)) {
+		dir = dir_flat;
+	} else {
+		return console.log("grunt-three-obj tasks not found");
+	}
+	// add tasks
+	grunt.loadTasks( dir );
 
 };
+
+// helpers
+function isDir( dir ){
+	try {
+		fs.accessSync(dir, fs.F_OK);
+		// exists
+		return true;
+	} catch (e) {
+		// It isn't accessible
+		return false;
+	}
+}
